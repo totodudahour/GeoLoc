f58e3f0bddb5130f8d346e1bae8404149a9b5bbd
lyorix <totodudahour@0-mail.com>
2011-07-04 18:08:44 +0200

    GeoLoc: Next commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -13,19 +13,28 @@ import com.simplegeo.client.SimpleGeoContextClient;
 
 public class Application extends Controller {
 
-    private static final Logger logger = Logger.getLogger(Application.class);
+	private static final Logger logger = Logger.getLogger(Application.class);
 
-    public static void index() {
-	render();
-    }
-
-    public static void getLocation(double lat, double lon) {
-	SimpleGeoContextClient client = GeoLoc.getInstance().getClient();
-	try {
-	    Map<String, Object> map = client.getContext(lat, lon);
-	    renderJSON(map);
-	} catch (IOException e) {
-	    logger.fatal(e);
+	public static void index() {
+		render();
 	}
-    }
+
+	/**
+	 * Get the location context from the longitude and latitude values
+	 * 
+	 * @param lat
+	 *            - the latitude
+	 * @param lon
+	 *            - the longitude
+	 */
+	public static void getLocation(double lat, double lon) {
+		SimpleGeoContextClient client = GeoLoc.getInstance().getClient();
+		try {
+			Map<String, Object> map = client.getContext(lat, lon);
+			System.out.println(map);
+			renderJSON(map);
+		} catch (IOException e) {
+			logger.fatal(e);
+		}
+	}
 }
--- app/models/GeoLoc.java
+++ app/models/GeoLoc.java
@@ -4,27 +4,37 @@ import com.simplegeo.client.SimpleGeoContextClient;
 
 public class GeoLoc {
 
-    private static GeoLoc model;
+	private static GeoLoc model;
 
-    private static final String KEY = "X6KaFHCvJxPMdvzvp4Y6kbKbhJPeA7vq";
-    private static final String SECRET = "SE7W5gmB3gLG2eNAxhUbk6M6mA7FUT99";
+	private static final String KEY = "X6KaFHCvJxPMdvzvp4Y6kbKbhJPeA7vq";
+	private static final String SECRET = "SE7W5gmB3gLG2eNAxhUbk6M6mA7FUT99";
 
-    private SimpleGeoContextClient client;
+	private SimpleGeoContextClient client;
 
-    private GeoLoc() {
-	super();
-	client = SimpleGeoContextClient.getInstance();
-	client.getHttpClient().setToken(KEY, SECRET);
-    }
-
-    public static GeoLoc getInstance() {
-	if (model == null) {
-	    model = new GeoLoc();
+	private GeoLoc() {
+		super();
+		client = SimpleGeoContextClient.getInstance();
+		client.getHttpClient().setToken(KEY, SECRET);
 	}
-	return model;
-    }
 
-    public SimpleGeoContextClient getClient() {
-	return client;
-    }
+	/**
+	 * Get an instance of the GeoLoc model with its key and secret
+	 * 
+	 * @return the GeoLoc instance
+	 */
+	public static GeoLoc getInstance() {
+		if (model == null) {
+			model = new GeoLoc();
+		}
+		return model;
+	}
+
+	/**
+	 * Get the simplegeo client
+	 * 
+	 * @return the simplegeo client
+	 */
+	public SimpleGeoContextClient getClient() {
+		return client;
+	}
 }
